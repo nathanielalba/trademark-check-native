4a6eded7a17e8e9c65992bf1cc5ed2bac5f5f14d
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-10-11 15:22:10 -0700

    add app file for ios and android

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class BusinessDemo extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello There</Text>
+      </View>
+    );
+  }
+}
